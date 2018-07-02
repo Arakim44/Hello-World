b549b9fa4acb4952e1892041a61c521d13ff0fd2
Ara Kim <ara@Aras-MacBook-Air.local>
2018-07-02 18:14:09 -0500

    First Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Ara Kim on 7/2/18.
+//  Copyright © 2018 Ara Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
